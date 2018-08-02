cac128483c8057d919c824242014ea5423353930
nilabha <nilabha@users.noreply.github.com>
2018-08-03 02:50:04 +0530

    Add files via upload
    Updated Files

--- supportbot/nlu_model.py
+++ supportbot/nlu_model.py
@@ -13,39 +13,27 @@ def train_nlu(data, configs, model_dir):
 	
 def run_nlu():
 	interpreter = Interpreter.load('./models/nlu/default/supportnlu')
-	#print(interpreter.parse("MySpace Password Expiry")['intent']['name'])
+	print(interpreter.parse("MySpace Password Expiry")['intent']['name'])
 
 	dfOutput = pd.read_csv('SampleOutput.csv')
 	dfAns = pd.read_csv('Answers.csv')
-	dictIntentMap = dict(zip(dfAns.intent,dfAns.Answers))	
-	#print(interpreter.parse("Login Page is not getting displayed"))
-	#print(interpreter.parse("Forgot the password, can you please reset the password for me."))	
+	dictIntentMap = dict(zip(dfAns.intent,dfAns.Answers))
 	for index1,row1 in dfOutput.iterrows():
 		reply = interpreter.parse(row1['Question'])['intent']['name']
-		#print(interpreter.parse(row1['Question']))
-		dfOutput.ix[index1,"Correct"]=(dictIntentMap.get(reply, "")==row1['Answers'])
-		dfOutput.ix[index1,"Reply"]=dictIntentMap.get(reply, "")
+		dfOutput.ix[index1,"Correct"]=(dictIntentMap.get(reply, "No Answer Found")==row1['Answers'])
+		dfOutput.ix[index1,"Reply"]=dictIntentMap.get(reply, "No Answer Found")
 	print("Test Set Accuracy:{0:.2%}".format(np.sum(dfOutput['Correct'])/dfOutput.shape[0]))
 	dfOutput.to_csv('OutputReply.csv')
 
 def run_nlu_train():
 	interpreter = Interpreter.load('./models/nlu/default/supportnlu')
-	#print(interpreter.parse("MySpace Password Expiry")['intent']['name'])
-
 	dfInput = pd.read_csv('SampleInput.csv')
 	dfAns = pd.read_csv('Answers.csv')
 	dictIntentMap = dict(zip(dfAns.intent,dfAns.Answers))	
-	#print(interpreter.parse("Login Page is not getting displayed"))
-	#print(interpreter.parse("Forgot the password, can you please reset the password for me."))	
-	for index1,row1 in dfInput.iterrows():        
-		#print(interpreter.parse(row1['Question']))
-
+	for index1,row1 in dfInput.iterrows():
 		reply = interpreter.parse(row1['Question'])['intent']['name']
-		#print("reply:",dictIntentMap[reply])
-		#print("answer:",row1['Answers'])
-		#print(dictIntentMap[reply]==row1['Answers'])
-		dfInput.ix[index1,"Reply"]= dictIntentMap.get(reply, "")
-		dfInput.ix[index1,"Correct"]=(dictIntentMap.get(reply, "")==row1['Answers'])
+		dfInput.ix[index1,"Reply"]= dictIntentMap.get(reply, "No Answer Found")
+		dfInput.ix[index1,"Correct"]=(dictIntentMap.get(reply, "No Answer Found")==row1['Answers'])
 	print("Train Set Accuracy:{0:.2%}".format(np.sum(dfInput['Correct'])/dfInput.shape[0]))
 	dfInput.to_csv('InputReply.csv')
 
